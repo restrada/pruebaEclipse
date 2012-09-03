83765311f00471d19c7dd92e2694e3e884fe20f4
Rodrigo Espíndola Estrada <spajx@hotmail.com>
2012-09-03 12:18:06 -0500

    Primeros cambios en Eclipse con el Plug

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
 
 	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int c = a + b;
 		System.out.println("¡Hola Mundo!");
-
+		System.out.println("La Suma de " + a + " + " + b + " = "+ c);
 	}
 
 }
