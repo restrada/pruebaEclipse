36c60d73220d737d893d3474b8a23e60a82daac8
ALMACENES ANFORA <ALMACENES ANFORA@ALMACENESANFORA>
2012-09-03 13:09:11 -0500

    Subo nuevos cambios

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 2;
+		int a = 9;
+		int b = 6;
 		int c = a + b;
+		int d = a + b;
 		System.out.println("¡Hola Mundo!");
-		System.out.println("La Suma de " + a + " + " + b + " = "+ c);
+		System.out.println("La Suma de " + a + " + " + b + " = " + c);
+		System.out.println("La Multiplicación de " + a + " * " + b + " = " + d);
 	}
 
 }
